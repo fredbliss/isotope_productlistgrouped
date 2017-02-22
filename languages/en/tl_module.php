8854899a9218113a88f7ed6dc6d21e41b3dc058d
fbliss <fred.bliss@intelligentspark.com>
2017-02-22 00:41:24 -0500

    added modules.xlf added tl_module.xlf removed tl_module.php from language files (en)

--- languages/en/tl_module.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$GLOBALS['TL_LANG']['tl_module']['iso_perGroup'] = array('Products per group','Specify how many products in a given group.');
-$GLOBALS['TL_LANG']['tl_module']['iso_custom_categories'] = array('Categories','Select one or more categories that you would like to show in this product list.');
-
-$GLOBALS['TL_LANG']['tl_module']['iso_category_scope_ref']['custom'] = array('Custom','Select one or more cateogries to include.');
