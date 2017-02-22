bbdceee283acebef4d2996c6063d204f82ced36f
fbliss <fred.bliss@intelligentspark.com>
2017-02-22 00:33:55 -0500

    added language file for tl_module entries

---
+++ languages/en/tl_module.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$GLOBALS['TL_LANG']['tl_module']['iso_perGroup'] = array('Products per group','Specify how many products in a given group.');
+$GLOBALS['TL_LANG']['tl_module']['iso_custom_categories'] = array('Categories','Select one or more categories that you would like to show in this product list.');
+
+$GLOBALS['TL_LANG']['tl_module']['iso_category_scope_ref']['custom'] = array('Custom','Select one or more cateogries to include.');
